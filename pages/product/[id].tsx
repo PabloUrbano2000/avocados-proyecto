a71525ac4379e4af4b2933f3f801024bf35df6ee
Pablo Urbano <pablojamiro2008@hotmail.com>
2022-07-29 16:18:37 -0500

    udapte

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,23 +1,59 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
 
 import Layout from '@components/Layout/Layout'
 import ProductSummary from '@components/ProductSummary/ProductSummary'
 
-const ProductPage = () => {
-  const { query } = useRouter()
-  const [product, setProduct] = useState<TProduct | null>(null)
+export const getStaticPaths = async () => {
+  const response = await fetch(
+    'https://avocados-proyecto-mgpiy9zhc-pablitoelmaldy.vercel.app/api/avo'
+  )
+  const { data }: TAPIAvoResponse = await response.json()
 
-  useEffect(() => {
-    if (query.id) {
-      window
-        .fetch(`/api/avo/${query.id}`)
-        .then((response) => response.json())
-        .then((data: TProduct) => {
-          setProduct(data)
-        })
-    }
-  }, [query.id])
+  const paths = data.map((avo) => ({
+    params: {
+      id: avo.id,
+    },
+  }))
+
+  return {
+    paths: paths,
+    // incremental static generation
+    // 404 for everything generation
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // pages only
+  const id = params?.id as string
+  const response = await fetch(
+    `https://avocados-proyecto-mgpiy9zhc-pablitoelmaldy.vercel.app/api/avo/${id}`
+  )
+  const data: TProduct = await response.json()
+
+  return {
+    props: {
+      product: data,
+    },
+  }
+}
+
+const ProductPage = ({ product }: { product: TProduct }) => {
+  // const { query } = useRouter()
+  // const [product, setProduct] = useState<TProduct | null>(null)
+
+  // useEffect(() => {
+  //   if (query.id) {
+  //     window
+  //       .fetch(`/api/avo/${query.id}`)
+  //       .then((response) => response.json())
+  //       .then((data: TProduct) => {
+  //         setProduct(data)
+  //       })
+  //   }
+  // }, [query.id])
 
   return (
     <Layout>
